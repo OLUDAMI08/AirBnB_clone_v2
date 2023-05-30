d82b61ea345f227de5b9276545e1b538de7323a4
Lawal Damilare.Toheeb <lawaltd088@gmail.com>
2023-05-30 20:18:23 +0000

    distributes an archive to the web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,64 +1,56 @@
 #!/usr/bin/python3
-"""Distributes an archive to your web servers"""
-import os
+"""Compress web static package
+"""
 from fabric.api import *
 from datetime import datetime
+from os import path
 
 
-def do_pack():
-    """Create a tar gzipped archive of the directory web_static"""
-    try:
-        dt = datetime.now()
-        file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                                  dt.month,
-                                                                  dt.day,
-                                                                  dt.hour,
-                                                                  dt.minute,
-                                                                  dt.second)
-        if not os.path.isdir("versions"):
-            local("mkdir versions")
+env.hosts = ['54.236.54.193', '34.201.61.125']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
-        local("tar -cvzf {} web_static/*".format(file_name))
-        return file_name
-    except Exception:
-        return None
-
-
-env.hosts = ['54.146.81.195', '34.224.6.180']
 
 def do_deploy(archive_path):
-    """deploys the archive to the servers and updates it"""
-    if not os.path.isfile(archive_path):
-        return False
+        """Deploy web files to server
+        """
+        try:
+                if not (path.exists(archive_path)):
+                        return False
 
-    try:
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, '/tmp/')
+                # upload archive
+                put(archive_path, '/tmp/')
 
-        # Uncompress the archive to the folder
-        # /data/web_static/releases/<archive filename without extension>
-        # on the web server
-        filename = os.path.basename(archive_path)
-        fname = filename.split('.')[0]
-        folder_name = '/data/web_static/releases/' + fname
-        run('mkdir -p {}'.format(folder_name))
-        run('tar -xzf /tmp/{} -C {}'.format(filename, folder_name))
+                # create target dir
+                timestamp = archive_path[-18:-4]
+                run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
 
-        # Delete the archive from the web server
-        run('rm /tmp/{}'.format(filename))
+                # uncompress archive and delete .tgz
+                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+/data/web_static/releases/web_static_{}/'
+                    .format(timestamp, timestamp))
 
-        # Move content out of the sub-folder
-        run("mv /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}/".format(fname, fname))
+                # remove archive
+                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
-        # Delete the symbolic link /data/web_static/current from the web server
-        run('rm -rf /data/web_static/current')
+                # move contents into host web_static
+                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
 
-        # Create a new the symbolic link /data/web_static/current on the
-        # web server linked to the new version of your code
-        # (/data/web_static/releases/<archive filename without extension>)
-        run('ln -s {} /data/web_static/current'.format(folder_name))
+                # remove extraneous web_static dir
+                run('sudo rm -rf /data/web_static/releases/\
+web_static_{}/web_static'
+                    .format(timestamp))
 
+                # delete pre-existing sym link
+                run('sudo rm -rf /data/web_static/current')
+
+                # re-establish symbolic link
+                run('sudo ln -s /data/web_static/releases/\
+web_static_{}/ /data/web_static/current'.format(timestamp))
+        except:
+                return False
+
+        # return True on success
         return True
-    except Exception:
-        return False
